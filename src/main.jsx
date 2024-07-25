3eb14ddff1bb5bd541a9fb9f5344c1f4dac109a0
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-25 07:36:51 +0600

    add axios and react query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Router } from './Router/Route.jsx'
 import { HelmetProvider } from 'react-helmet-async'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={Router} />
+    </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>,
 )

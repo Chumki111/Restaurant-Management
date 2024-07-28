a860781e843a4377a33a92f1e546f7b0eeb4fca2
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-28 08:03:53 +0600

    gallery page design complete

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,8 +1,14 @@
 import { Helmet } from "react-helmet-async"
 import PrimaryHero from "../../Components/Hero/PrimaryHero"
+import { useQuery } from "@tanstack/react-query"
+import { getGallery } from "../../api/gallery"
 
 
 const Gallery = () => {
+    const {data:galleries} = useQuery({
+        queryKey:['galleries'],
+        queryFn:async() => getGallery()
+    })
     return (
         <>
             {/* helmet */}
@@ -13,19 +19,28 @@ const Gallery = () => {
             <PrimaryHero text="Gallery" />
             <div className="flex flex-col">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                {/* <button
+                        // onClick={handleAddClick}
+                        className="bg-primary text-white py-2 px-4 rounded mb-6"
+                    >
+                        Add
+                    </button> */}
                     {/* images */}
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-12 ">
-                        <img src="https://i.ibb.co/jV2jZk2/download-1.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/4jPrSpM/download-4.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/PhdYwps/download-3.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/w6ztdrg/download-6.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/P5bDMKN/download-7.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/QHCDjHK/download-8.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/M6YR8rc/download-2.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/mq0fBRg/download-9.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/yR4kYcR/download-10.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/TrdpxPz/download-13.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
-                        <img src="https://i.ibb.co/mq0fBRg/download-9.jpg" alt="" className="rounded-md transition duration-300 hover:scale-105 w-[270px] h-[190px] object-fill" />
+                        {/* gallery map */}
+                        {galleries?.map((gallery) => (
+                            <div key={gallery.id} className="relative">
+                                <img
+                                    src={gallery.image}
+                                    alt={gallery.name}
+                                    className="w-full h-60 object-cover rounded-lg shadow-lg"
+                                />
+                                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-white p-4 rounded-lg">
+                                    <h3 className="text-lg font-semibold">{gallery.name}</h3>
+                                    <p className="mt-2">{gallery.feedback}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>

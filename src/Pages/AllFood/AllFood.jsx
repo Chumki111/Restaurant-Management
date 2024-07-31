67f496820f515d91f7e5e9d24b32c3cf3efac149
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-31 08:35:23 +0600

    single page design

--- src/Pages/AllFood/AllFood.jsx
+++ src/Pages/AllFood/AllFood.jsx
@@ -3,9 +3,9 @@ import { useQuery } from '@tanstack/react-query';
 import { Helmet } from "react-helmet-async";
 import PrimaryHero from "../../Components/Hero/PrimaryHero";
 import { getAllFoods } from '../../api/foods';
-import FoodCard from '../../Components/Card/FoodCard';
 import Loading from '../../Components/Loading/Loading';
 import Error from '../../Components/Loading/Error';
+import TopFoodCard from '../../Components/Card/TopFoodCard';
 
 const AllFood = () => {
   const [search, setSearch] = useState('');
@@ -24,6 +24,11 @@ const AllFood = () => {
     return () => clearTimeout(delayDebounceFn);
   }, [search, refetch]);
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    refetch();
+  };
+
   return (
     <>
       <Helmet>
@@ -31,22 +36,24 @@ const AllFood = () => {
       </Helmet>
       <PrimaryHero text="All Foods" />
       <div className="container mx-auto p-4">
-        <div className="flex justify-center items-start">
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="Search for food... by food name"
-          className="mb-4 p-3 border-2 rounded-lg w-full md:w-3/4 lg:w-1/2 focus:outline-none focus:ring-2 focus:ring-secondary focus:border-transparent mt-7 transition-all duration-300 ease-in-out transform focus:scale-105 shadow-lg"
-        />
-        </div>
-        
+        <form onSubmit={handleSearchSubmit} className="flex justify-center items-start">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search for food... by food name"
+            className="mb-4 p-3 border-2 rounded-lg w-full md:w-3/4 lg:w-1/2 focus:outline-none focus:ring-2 focus:ring-secondary focus:border-transparent mt-7 transition-all duration-300 ease-in-out transform focus:scale-105 shadow-lg"
+          />
+        </form>
+
         {isLoading ? (
           <Loading />
         ) : (
           <>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-16">
-              {foods?.map((food) => <FoodCard key={food._id} food={food} />)}
+              {foods?.map((food) => (
+                <TopFoodCard key={food._id} food={food} />
+              ))}
             </div>
             {foods?.length === 0 && (
               <p className="text-center text-gray-500 mt-4">No foods found</p>

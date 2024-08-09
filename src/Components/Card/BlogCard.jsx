e4e4e8368521b5b93212d5e4fb5231f60d23786c
Chumki khatun <chumkikhatun370@gmail.com>
2024-08-09 12:58:21 +0600

    change blog card shadow color

--- src/Components/Card/BlogCard.jsx
+++ src/Components/Card/BlogCard.jsx
@@ -1,24 +1,29 @@
-import { Link } from "react-router-dom"
-import PrimaryButton from "../Button/PrimaryButton"
+import PrimaryButton from "../Button/PrimaryButton";
 
-
-const BlogCard = ({blog}) => {
+const BlogCard = ({ blog }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden p-4 space-y-3">
-    <img className="w-full h-60 object-cover rounded transition ease-in-out transform duration-300 hover:scale-105" src={blog.image} alt={blog.title} />
-    <div className="">
-      <h2 className="text-xl font-bold text-gray-800">{blog.title}</h2>
-      <p className="text-gray-600 pt-3">
-        {blog?.content?.length > 100 ? blog?.content?.substring(0,100) + "....." : blog?.content}
+    <div 
+      className="bg-white shadow-[0_4px_10px_rgba(50,205,50,0.5)] rounded-lg overflow-hidden p-4 space-y-3 transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
+      data-aos="fade-up"
+    >
+      <img 
+        className="w-full h-60 object-cover rounded transition ease-in-out transform duration-300 hover:scale-110" 
+        src={blog.image} 
+        alt={blog.title} 
+      />
+      <div className="">
+        <h2 className="text-xl font-bold text-gray-800 transition-colors duration-300 hover:text-secondary">
+          {blog.title}
+        </h2>
+        <p className="text-gray-600 pt-3">
+          {blog?.content?.length > 100 ? blog?.content?.substring(0, 100) + "....." : blog?.content}
         </p>
+      </div>
+      <div className="pt-4">
+        <PrimaryButton text={'Read More'} />
+      </div>
     </div>
-    <div className="">
- 
-   <PrimaryButton text={'Read More'}/>
-   
-    </div>
-  </div>
-  )
+  );
 }
 
-export default BlogCard
+export default BlogCard;
--- src/Components/Home/AboutSection.jsx
+++ src/Components/Home/AboutSection.jsx
@@ -18,8 +18,8 @@ const AboutSection = () => {
             <img
               src="https://i.postimg.cc/Dzh0ZWry/close-up-appetizing-ramadan-meal-23-2151182546.jpg"
               alt="Company Values"
-              className="inset-0 w-full h-full object-cover rounded-md transition ease-in-out transform duration-300 hover:scale-105"
-            />
+              className="inset-0 w-full h-full object-cover rounded-md transition ease-in-out transform duration-300 hover:scale-105 animate-slide-in"
+              data-aos="fade-up"/>
           </div>
         </div>
 

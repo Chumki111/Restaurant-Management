21a4dec0189e728ab68ea157b063afc53e4d938d
Chumki khatun <chumkikhatun370@gmail.com>
2024-08-09 10:24:16 +0600

    add banner icon

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,13 +1,14 @@
+import { FaCarrot, FaAppleAlt, FaPepperHot, FaLemon } from 'react-icons/fa';
 import image from '../../assets/grilled-lamb.png';
 import SecondaryButton from '../Button/SecondaryButton';
 
 const Banner = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className='flex flex-col lg:flex-row min-h-[600px] justify-center items-center space-x-4'>
+      <div className='flex flex-col lg:flex-row justify-center items-center space-x-4 relative'>
         
         {/* Text Section */}
-        <div>
+        <div className='relative lg:w-1/2' data-aos="fade-up">
           <h1 className='text-4xl lg:text-5xl font-bold mb-4 bg-gradient-to-r from-secondary to-tertiary text-transparent bg-clip-text'>
             Elevate Your Dining Experience
           </h1>
@@ -17,15 +18,36 @@ const Banner = () => {
           <div>
             <SecondaryButton text={'Explore Now'} to="/all-foods" />
           </div>
+
+          {/* Vegetable Icon Below Button */}
+          <div className='mt-6 flex justify-center'>
+            <FaCarrot className='text-secondary text-4xl transform hover:scale-110 transition-transform duration-500 ease-in-out' />
+          </div>
+
+          {/* Vegetable Icons */}
+          <div className='absolute   flex space-x-4'>
+            <FaAppleAlt className='text-secondary text-3xl transform hover:scale-110 transition-transform duration-500 ease-in-out' />
+          </div>
+          <div className='absolute top-4 right-4 flex space-x-4'>
+            <FaPepperHot className='text-secondary text-3xl transform hover:scale-110 transition-transform duration-500 ease-in-out' />
+          </div>
         </div>
         
         {/* Image Section */}
-        <div className='w-[90%] h-full lg:w-[80%]'>
+        <div className='w-full lg:w-1/2 h-full relative'>
           <img 
             src={image} 
             alt="Banner" 
             className='w-full h-full object-cover transform animate-slide-in'
           />
+           
+          {/* Icons in Image Section */}
+          <div className='absolute bottom-4 right-4 flex space-x-4' data-aos="fade-up">
+            <FaLemon className='text-secondary text-3xl transform hover:scale-110 transition-transform duration-500 ease-in-out' />
+          </div>
+          <div className='absolute bottom-4 left-4 flex space-x-4' data-aos="fade-up">
+            <FaAppleAlt className='text-secondary text-3xl transform hover:scale-110 transition-transform duration-500 ease-in-out' />
+          </div>
         </div>
       </div>
     </div>

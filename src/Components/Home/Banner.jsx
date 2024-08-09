a86f47f61bfe3a9dd1d6bda39582a6a0f9080536
Chumki khatun <chumkikhatun370@gmail.com>
2024-08-09 09:56:18 +0600

    add banner image animation

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,28 +1,32 @@
-import image from '../../assets/banner.jpg';
+import image from '../../assets/grilled-lamb.png';
 import SecondaryButton from '../Button/SecondaryButton';
 
 const Banner = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className='flex flex-col lg:flex-row min-h-[600px] mt-16'>
-         {/* Text Section */}
-         <div className='w-full lg:w-1/2 flex flex-col'>
+      <div className='flex flex-col lg:flex-row min-h-[600px] justify-center items-center space-x-4'>
+        
+        {/* Text Section */}
+        <div>
           <h1 className='text-4xl lg:text-5xl font-bold mb-4 bg-gradient-to-r from-secondary to-tertiary text-transparent bg-clip-text'>
             Elevate Your Dining Experience
           </h1>
-          <p className='text-lg mb-8'>
+          <p className='text-lg mb-8 max-w-md'>
             Indulge in the finest culinary delights and impeccable service that will make your dining unforgettable.
           </p>
           <div>
-           <SecondaryButton text={'Explore Now'} to="/all-foods"/>
+            <SecondaryButton text={'Explore Now'} to="/all-foods" />
           </div>
         </div>
+        
         {/* Image Section */}
-        <div className='w-full lg:w-1/2'>
-          <img src={image} alt="Banner" className='w-full h-full object-cover' />
+        <div className='w-[90%] h-full lg:w-[80%]'>
+          <img 
+            src={image} 
+            alt="Banner" 
+            className='w-full h-full object-cover transform animate-slide-in'
+          />
         </div>
-
-       
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,16 @@ export default {
         secondary:'#FFD700',
         tertiary:'#32CD32',
         primary_dark: '#E55347',
-      }
+      },
+      keyframes: {
+        'slide-in': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 1s ease-out forwards',
+      },
     },
   },
   plugins: [],
